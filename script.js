6232c40bf9d61a57ec659a686cf018f14e12dc37
AyaanMohiuddin <ayaanmohiuddin77@gmail.com>
2026-08-17 15:44:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const images = document.querySelectorAll('.image');
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById('lightbox-img');
+
+images.forEach(image => {
+
+    image.addEventListener('click', () => {
+
+        lightbox.style.display = 'flex';
+
+        lightboxImg.src = image.src;
+
+    });
+
+});
+
+lightbox.addEventListener('click', () => {
+
+    lightbox.style.display = 'none';
+
+});
+const topBtn = document.querySelector('.top-btn');
+
+window.addEventListener('scroll', () => {
+
+    if (window.scrollY > 200) {
+
+        topBtn.style.display = 'block';
+
+    } else {
+
+        topBtn.style.display = 'none';
+
+    }
+
+});
